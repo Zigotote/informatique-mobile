25a43a680a5f4b8aa2ae5553047140611ee194cf
Zigotote <oce88b@gmail.com>
2020-10-30 16:42:46 +0100

    Début conversion monnaie

---
+++ 07_flutter_convertisseur/convertisseur_devises/lib/pages/convertisseur_page.dart
@@ -0,0 +1,89 @@
+import 'package:convertisseur_devises/models/devise.dart';
+import 'package:convertisseur_devises/styles.dart';
+import 'package:flutter/material.dart';
+
+class ConvertisseurDevisePage extends StatefulWidget { 
+  ConvertisseurDevisePage();
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ConvertisseurDevisePage();
+  }
+}
+
+class _ConvertisseurDevisePage extends State<ConvertisseurDevisePage> {
+
+  // les différents "états" de la page
+  double _valeur; // valeur saisie
+  Devise _deviseInitial; // devise initiale sélectionnée
+  Devise _deviseFinale; // devise finale sélectionnée
+  double _resultat; // le résultat de la conversion
+  
+  // définition des valeurs initiales
+  @override
+  void initState() {
+    super.initState();
+    _valeur = 0;
+    _resultat = 0;
+    _deviseInitial = Devise.EURO;
+    _deviseFinale = Devise.DOLLAR;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+      children: [
+        Spacer(),
+        Text(
+          'Valeur',
+          style: AppStyle.labelStyle,
+        ),
+        Spacer(),
+        TextField( 
+          style: AppStyle.inputStyle,
+          onChanged: (saisie){
+            setState(() {
+              _valeur = double.parse(saisie);
+            });
+          },
+        ),
+        Spacer(),
+        Text(
+          'De',
+          style: AppStyle.labelStyle,
+        ),Spacer(),
+        DropdownButton(
+          isExpanded: true,
+          value: _deviseInitial,
+          onChanged: (newVal) {
+            setState(() {
+              _deviseInitial = newVal;
+            });
+          },
+          items: Devise.values.map((e) => DropdownMenuItem<Devise>(child: Text(e.libelle), value: e)).toList()),
+        Spacer(),
+        Text('Vers', style: AppStyle.labelStyle),
+        Spacer(),
+        DropdownButton(
+          isExpanded: true,
+          value: _deviseFinale,
+          onChanged: (newVal) {
+            setState(() {
+              _deviseFinale = newVal;
+            });
+          },
+          items: Devise.values.map((e) => DropdownMenuItem<Devise>(child: Text(e.libelle), value: e)).toList()),
+        Spacer(
+          flex: 2,
+        ),
+        ElevatedButton(onPressed: () => true, child: Text('Convertir')),
+        Spacer(
+          flex: 2,
+        ),
+        Text(_resultat.toString(), style: AppStyle.labelStyle),
+        Spacer(),
+      ],
+    ));
+  }
+}
